088026373ccbfc30c53a142967a7e5f5e890d5ea
Mario Muñoz <6872914-mariomunoz78@users.noreply.gitlab.com>
2021-04-18 15:03:18 -0500

    conexion a la base de datos

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //para configurar el servidor express
-
 //hacemos una constante y llamamos al modulo express
-
 import express from 'express';
-//const express=require('express');
 import morgan from 'morgan';
-//const morgan=require('morgan');
 import cors from 'cors';
-//const cors=require('cors');
+import path from 'path';
+import mongoose from 'mongoose';
+
+
+mongoose.Promise=global.Promise;
+//la cadena de conexion a la base de datos mongodb
+const dbUrl='mongodb://localhost:27017/dbsistema';
+mongoose.connect(dbUrl, {useUnifiedTopology:true, useNewUrlParser: true})
+.then(() => console.log('DB Connected puerto 27017!'))
+.catch(err => {
+console.log('error mario');
+});
+// si todo es exitoso que nos muestre un mensaje si no que nos muestre el error
 
 //declaramos un objeto que va hacer una instancia a express
 const app=express();
@@ -16,6 +24,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+//ruta publica de inicio
+app.use(express.static(path.join(__dirname,'public')));
 
 //que tome el puerto asignado por el servicio SI NO que le asigne el puerto 3000
 app.set('port',process.env.PORT || 3000);
@@ -23,4 +33,5 @@ app.set('port',process.env.PORT || 3000);
 
 app.listen(app.get('port') ,()=>{
     console.log('EL SERVIDOR'+app.get('port'));
+    // console.log(path.join(__dirname,'public'));
 })
