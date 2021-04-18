de9ba7d4e708c14e4292d2a1826d1df065011b51
Mario Muñoz <6872914-mariomunoz78@users.noreply.gitlab.com>
2021-04-18 12:28:15 -0500

    add morgan and cors middlewares

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 
 //hacemos una constante y llamamos al modulo express
 const express=require('express');
+const morgan=require('morgan');
+const cors=require('cors');
+
 //declaramos un objeto que va hacer una instancia a express
 const app=express();
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 //que tome el puerto asignado por el servicio SI NO que le asigne el puerto 3000
 app.set('port',process.env.PORT || 3000);
 
