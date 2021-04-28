d80e8944957b5188d7a6c5830bbdec8ba2adcf54
Mario Muñoz <6872914-mariomunoz78@users.noreply.gitlab.com>
2021-04-27 23:05:05 -0500

    modelo usuario

---
+++ models/usuario.js
@@ -0,0 +1,18 @@
+import mongoose,{Schema} from 'mongoose';
+
+const usuarioSchema = new Schema({
+    rol:{type:String, maxlength:50,unique:true,required:true},
+    nombre: {type:String,unique:true,maxlength:50},
+    tipo_documento: {type:String,maxlength:50},
+    num_documento: {type:String,maxlength:50},
+    direccion: {type:String,maxlength:50},
+    telefono: {type:String,maxlength:50},
+    email: {type:String,unique:true,maxlength:50},
+    password: {type:String,maxlength:200}, 
+    estado: {type:Number,default:1},
+    createdAt:{type:Date,default:Date.now}
+});
+
+const Usuario = mongoose.model('usuario',usuarioSchema);
+
+export default Usuario;
