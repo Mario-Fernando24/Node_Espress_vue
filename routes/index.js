2555294292075b819bf02bfd1ee2f80aef7bbc62
Mario Muñoz <6872914-mariomunoz78@users.noreply.gitlab.com>
2021-04-27 23:27:41 -0500

    routes de los usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 import routerx from 'express-promise-router';
 import categoriaRouter from './categoria';
 import articuloRouter from './articulo';
+import usuarioRouter from './usuario';
+
 
 const router =routerx();
 
 //cuando se haga referencia a la url 
 router.use('/categoria',categoriaRouter);
 router.use('/articulo',articuloRouter);
+router.use('/usuario',usuarioRouter);
 
 export default router; 
