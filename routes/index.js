5ad32f5151fa5a00072305c8a130527a38c87ef3
Mario Muñoz <6872914-mariomunoz78@users.noreply.gitlab.com>
2021-05-09 03:10:38 -0500

    add routes ingresos

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import categoriaRouter from './categoria';
 import articuloRouter from './articulo';
 import usuarioRouter from './usuario';
 import personaRouter from './persona';
+import ingresoRouter from './ingreso';
 
 const router =routerx();
 
@@ -11,5 +12,6 @@ router.use('/categoria',categoriaRouter);
 router.use('/articulo',articuloRouter);
 router.use('/usuario',usuarioRouter);
 router.use('/persona',personaRouter);
+router.use('/ingreso',ingresoRouter);
 
 export default router; 
---
+++ routes/ingreso.js
@@ -0,0 +1,18 @@
+import routerx from 'express-promise-router';
+import ingresoController from '../controllers/IngresoController';
+//importo el middlewares
+import auth from '../middlewares/auth';
+
+const router=routerx();
+
+//solamente los administrador van a poder agregar nuevos usuarios
+
+router.post('/add',auth.verifyAlmacenero,ingresoController.add);
+router.get('/query',auth.verifyAlmacenero,ingresoController.query);
+router.get('/list',auth.verifyAlmacenero,ingresoController.list);
+
+
+router.put('/activate',auth.verifyAlmacenero,ingresoController.activate);
+router.put('/deactivate',auth.verifyAlmacenero,ingresoController.desactivate);
+
+export default router;
